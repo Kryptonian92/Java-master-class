a1989f4a969277cfecc67c82ee69f8fcfbbf5161
Kryptonian92 <ausar_mcgruder@yahoo.com>
2019-10-19 04:07:26 -0400

    Primitive tpes challenge

--- ByteShortIntLong/src/academy/learnprogramming/PrimitiveChallenge.java
+++ ByteShortIntLong/src/academy/learnprogramming/PrimitiveChallenge.java
@@ -1,4 +1,13 @@
 package academy.learnprogramming;
 
 public class PrimitiveChallenge {
+    public static void main(String[] args){
+
+        byte byteNum = (byte) 10;
+        short shortNum = (short) 20;
+        int intNum = 50;
+        long longNum = 50000 + ((byteNum + shortNum + intNum) * 10);
+
+        System.out.println("Long number = " + longNum);
+    }
 }
