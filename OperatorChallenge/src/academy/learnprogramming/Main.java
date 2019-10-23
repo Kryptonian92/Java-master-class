ae8ce05954f24b9eb72cb6e253b174260219951e
Kryptonian92 <ausar_mcgruder@yahoo.com>
2019-10-23 19:31:34 -0400

    updating master with latest code

---
+++ OperatorChallenge/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+import java.util.ListResourceBundle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double doubleNum1 = 10.00d;
+        double doubleNum2 = 10.00d;
+        double doubleFinal = (doubleNum1 + doubleNum2)*10;
+        System.out.println(doubleFinal);
+
+        if (doubleFinal%2 == 0){
+            boolean isRemainder = false;
+        } else{
+            boolean isRemainder = true;
+        }
+
+    }
+
+}
--- Operators/src/academy/learnprogramming/Main.java
+++ Operators/src/academy/learnprogramming/Main.java
@@ -13,5 +13,36 @@ public class Main {
 
         result = result % 3; // remainder of (4 % 3);
         System.out.println("4 % 3 = " + result );
+
+        boolean isAlien = false;
+        if(isAlien == false){
+            System.out.println("It is an alien");
+        }
+
+        int topScore = 100;
+        if (topScore == 100 || topScore != 100){
+            System.out.println("You got got the top score");
+        }
+
+        int secondScore = 60;
+        if (topScore > secondScore && topScore < 100){
+            System.out.println("Greater than the second score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondScore <=90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newVal = 50;
+        if( newVal == 50){
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if(isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+        boolean wasCar = isCar ? true:false;
     }
 }
